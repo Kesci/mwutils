adf09ccf52f17b9dff0d8cef14efaeaa252b5b35
gerardtao <gerardtao0315@kesci.com>
2021-04-26 11:53:42 +0800

    4.7.1rc1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mwutils",  # Replace with your own username
-    version="0.4.7.1",
+    version="0.4.7-rc1",
     author="heywhale.com",
     description="use in mw",
     url="https://github.com/kesci/mwutils",
